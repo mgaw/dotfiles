b806b88da837df934b96a38f3b1f8e8a74633297
Marius Gawrisch <marius.gawrisch@mgaw.de>
2026-04-03 12:43:11 +0200

    Move direnv to mise

--- nix/direnv.nix
+++ nix/direnv.nix
@@ -1,17 +1,26 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
-  programs.direnv = {
-    enable = true;
-    enableZshIntegration = true;
-    silent = true;
-    config = {
-      warn_timeout = "0s";
-      hide_env_diff = true;
-    };
-    nix-direnv.enable = true;
-    stdlib = builtins.readFile ./run_if_changed.sh;
+  mise.tools.direnv = "latest";
+
+  xdg.configFile."direnv/direnv.toml".text = /* toml */ ''
+    [global]
+    warn_timeout = "0s"
+    hide_env_diff = true
+  '';
+
+  xdg.configFile."direnv/direnvrc".text = /* sh */ ''
+    source ${pkgs.nix-direnv}/share/nix-direnv/direnvrc
+    ${builtins.readFile ./run_if_changed.sh}
+  '';
+
+  home.sessionVariables = {
+    DIRENV_LOG_FORMAT = "";
   };
+
+  programs.zsh.initContent = lib.mkAfter /* zsh */ ''
+    eval "$(direnv hook zsh)"
+  '';
 
   programs.git.ignores = [
     "/.direnv"
